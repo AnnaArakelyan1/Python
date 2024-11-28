9416a264aeeff91298997573e81441276a9bb739
AnnaArakelyan1 <annaarakelyan.tt219-2@polytechnic.am>
2024-11-28 17:41:14 +0400

    Update and rename delete.py to vehicle.py

--- Homework11/delete.py
+++
@@ -1,6 +0,0 @@
-url1 = "https://jsonplaceholder.typicode.com/posts/100"
-response = requests.delete(url1)
-if response.status_code == 200:
-    print("Resource deleted successfully.")
-else:
-    print(f"Failed to delete resource. Status code: {response.status_code}, Reason: {response.reason}")
