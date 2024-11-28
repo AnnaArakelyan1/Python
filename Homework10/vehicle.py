a0e92dafc13eaafc4d9aa340774e6f85e957e64f
AnnaArakelyan1 <annaarakelyan.tt219-2@polytechnic.am>
2024-11-28 17:39:51 +0400

    Update and rename vehicle.py to delete.py

--- Homework10/vehicle.py
+++
@@ -1,9 +0,0 @@
-class Vehicle:
-    def __init__(self, make, year, color, mileage):
-        self.make = make
-        self.year = year
-        self.color = color
-        self.mileage = mileage
-
-    def __str__(self):
-        return f"Make: {self.make}, Year: {self.year}, Color: {self.color}, Mileage: {self.mileage} miles"
